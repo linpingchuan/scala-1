4b1636a7379006d61d8731d64c68ce17bf7fdca9
Erik Engbrecht <erik.engbrecht@gmail.com>
2009-09-27 13:41:19 -0400

    updated mutable.LinearSequence to inherit from mutable.LinearSequenceLike

--- src/library/scala/collection/mutable/LinearSequence.scala
+++ src/library/scala/collection/mutable/LinearSequence.scala
@@ -11,7 +11,8 @@
 
 package scala.collection.mutable
 
-import scala.collection.generic._
+import scala.collection.generic
+import scala.collection
 
 
 /** A subtrait of <code>collection.Sequence</code> which represents sequences
@@ -21,12 +22,12 @@ import scala.collection.generic._
  */
 trait LinearSequence[A] extends Sequence[A] 
                            with scala.collection.LinearSequence[A] 
-                           with GenericTraversableTemplate[A, LinearSequence]
-                           with scala.collection.LinearSequenceLike[A, LinearSequence[A]] {
-  override def companion: GenericCompanion[LinearSequence] = LinearSequence
+                           with generic.GenericTraversableTemplate[A, LinearSequence]
+                           with LinearSequenceLike[A, LinearSequence[A]] {
+  override def companion: generic.GenericCompanion[LinearSequence] = LinearSequence
 }
 
-object LinearSequence extends SequenceFactory[LinearSequence] {
-  implicit def builderFactory[A]: BuilderFactory[A, LinearSequence[A], Coll] = new VirtualBuilderFactory[A]
+object LinearSequence extends generic.SequenceFactory[LinearSequence] {
+  implicit def builderFactory[A]: generic.BuilderFactory[A, LinearSequence[A], Coll] = new VirtualBuilderFactory[A]
   def newBuilder[A]: Builder[A, LinearSequence[A]] = LinkedList.newBuilder[A]
 }
